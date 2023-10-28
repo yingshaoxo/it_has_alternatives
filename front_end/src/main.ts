17490c5517f72833b9fcce1db82600a701b7ebca
yingshaoxo <yingshaoxo@gmail.com>
2023-10-28 17:54:34 +0800

    update

--- front_end/src/main.ts
+++ front_end/src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 
 // old ie does not have fetch, that's why you use 'isomorphic-fetch' to let it support it
-import * as e6p from "es6-promise";
-(e6p as any).polyfill();
+//import * as e6p from "es6-promise";
+//(e6p as any).polyfill();
 import 'isomorphic-fetch';
 
 import './style.css'
